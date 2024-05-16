0769713161348dd5de69783c3bd43436ac92a4e4
Oleg Sidorov <oleg@sidorov.nl>
2024-05-16 21:58:44 +0200

    Add bookstore proto from the tests

--- token.go
+++ token.go
@@ -4,6 +4,32 @@ import "fmt"
 
 type TokenKind byte
 
+const (
+	TokenNone         TokenKind = iota
+	TokenAt           TokenKind = '@'
+	TokenDot          TokenKind = '.'
+	TokenDotDot       TokenKind = ':' // DotDot is a pseudo-token that represents a double dot.
+	TokenDoubleQuote  TokenKind = '"'
+	TokenEqual        TokenKind = '='
+	TokenGreater      TokenKind = '>'
+	TokenGreaterEqual TokenKind = 'G' // GreaterEqual is a pseudo-token that represents a greater than or equal operator.
+	TokenLBracket     TokenKind = '['
+	TokenLParen       TokenKind = '('
+	TokenLess         TokenKind = '<'
+	TokenLessEqual    TokenKind = 'L' // LessEqual is a pseudo-token that represents a less than or equal operator.
+	TokenMinus        TokenKind = '-'
+	TokenNode         TokenKind = 'N' // Node is a pseudo-token that represents a node.
+	TokenNumber       TokenKind = '0' // Number is a pseudo-token that represents a number.
+	TokenPipe         TokenKind = '|'
+	TokenRBracket     TokenKind = ']'
+	TokenRParen       TokenKind = ')'
+	TokenSingleQuote  TokenKind = '\''
+	TokenSlash        TokenKind = '/'
+	TokenSlashSlash   TokenKind = '\\' // SlashSlash is a pseudo-token that represents a double slash.
+	TokenStar         TokenKind = '*'
+	TokenString       TokenKind = 'S' // String is a pseudo-token that represents a string.
+)
+
 type Token struct {
 	Kind  TokenKind
 	Value string
@@ -17,32 +43,6 @@ func (t *Token) String() string {
 		return t.Value
 	}
 }
-
-const (
-	TokenNone         TokenKind = iota
-	TokenAt           TokenKind = '@'
-	TokenDot          TokenKind = '.'
-	TokenDotDot       TokenKind = ':' // DotDot is a pseudo-token that represents a double dot.
-	TokenDoubleQuote  TokenKind = '"'
-	TokenEqual        TokenKind = '='
-	TokenGreater      TokenKind = '>'
-	TokenGreaterEqual TokenKind = 'G'
-	TokenLBracket     TokenKind = '['
-	TokenLParen       TokenKind = '('
-	TokenLess         TokenKind = '<'
-	TokenLessEqual    TokenKind = 'L'
-	TokenMinus        TokenKind = '-'
-	TokenNode         TokenKind = 'N'
-	TokenNumber       TokenKind = '0'
-	TokenPipe         TokenKind = '|'
-	TokenRBracket     TokenKind = ']'
-	TokenRParen       TokenKind = ')'
-	TokenSingleQuote  TokenKind = '\''
-	TokenSlash        TokenKind = '/'
-	TokenSlashSlash   TokenKind = '\\' // SlashSlash is a pseudo-token that represents a double slash.
-	TokenStar         TokenKind = '*'
-	TokenString       TokenKind = 'S'
-)
 
 func NewToken(value string, kind TokenKind) *Token {
 	return &Token{Value: value, Kind: kind}
